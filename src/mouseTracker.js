9b69142b33e80154af8b8fdc89ff2a25c71bcb2a
Ján Bušfy <busfy.jan@gmail.com>
2015-05-26 12:13:54 +0200

    Added getDistanceTraveled to API
    Added getDistanceTraveled function to API and added functionality for
    storing user data via StorageManager

--- src/mouseTracker.js
+++ src/mouseTracker.js
@@ -12,6 +12,11 @@
 	};
 
 	/**
+	* Storage prefix for all keys used in storage
+	*/ 
+	var STORAGE_PREFIX = "mouseTracker-";
+
+	/**
 	* API - interface to interact with the plugin
 	* Instance of this class is returned to the user
 	* @param delegate - delegate for handling user requests
@@ -20,12 +25,14 @@
 		//this object
 		var _this = this;
 
+		//delegate handler
+		_this.delegate = delegate;
 
 		/**
 		* Test function for testing purposes
 		*/ 
-		_this.test = function(){
-			console.log("API TEST");
+		_this.getDistanceTraveled = function(){
+			return _this.delegate.getDistanceTraveled();
 		};
 	};
 
@@ -38,12 +45,13 @@
 		var _this = this;	
 		
 		//default config		
-		_this.config = DEFAULTS;
+		_this.config = DEFAULTS;				
 
-		//managers
-		_this.trackingManager = new TrackingManager();
-		_this.storageManager = new StorageManager();		
+		//actual distance in PX
+		_this.actualDistance = 0;
 
+		//utility helper
+		_this.utility = new Utility();
 
 		/**
 		* Initialization function
@@ -54,14 +62,17 @@
 			_this.updateConfig(config);			
 
 			//initialize mouse tracking manager
-			_this.trackingManager.init();
-
-			//initialize storage manager
-			_this.storageManager.init();
+			_this.trackingManager.init();			
 
 			//load storage data if remember_user is true
 			if(true === _this.config.remember_user){
-				console.log("REMEMBER");
+				var savedDistance = _this.storageManager.getKey("distanceTraveled");
+				if(false !== savedDistance){
+					_this.actualDistance = parseFloat(savedDistance);
+				}				
+			}
+			else{
+				_this.storageManager.deleteKey("distanceTraveled");
 			}			
 		};
 
@@ -76,22 +87,69 @@
 			}
 		}
 
+		/**
+		* Returns actual traveled distance in MM
+		* @return float - actual traveled distance
+		*/
+		_this.apiGetDistanceTraveled = function(){
+			return _this.utility.convertPixelsToMilimeters(_this.actualDistance);
+		};
+
+		/**
+		* Fired when distance in tracking manager changes		
+		* @param float gain - distance gain
+		*/
+		_this.trackingManagerDistanceChanged = function(gain){
+			_this.actualDistance += gain;			
+			
+			//save if remembering user
+			if(_this.config.remember_user){				
+				_this.storageManager.setKey("distanceTraveled", _this.actualDistance);
+			}
+		};
+
+		//delegate handler objects
+		_this.apiDelegate = {
+			getDistanceTraveled: apiGetDistanceTraveled
+		};
+
+		_this.trackingDelegate = {
+			distanceChanged: _this.trackingManagerDistanceChanged
+		};		
+
+		//managers
+		_this.trackingManager = new TrackingManager(_this.trackingDelegate, _this.utility);
+		_this.storageManager = new StorageManager();		
+
+		//initialize self with user filled config
 		_this.init(config);
-		return new API();	
+
+		//create and return API
+		return new API(_this.apiDelegate);	
 	};	
 
 	/**
 	* TrackingManager provides support for handling mouse events
+	* @param object delegate - events handler
+	* @param Utility utility - helper object
 	*/
-	function TrackingManager(){
+	function TrackingManager(delegate, utility){
 		//this object
 		var _this = this;
 
-		//utility
-		_this.utility = new Utility();
+		//delegate
+		_this.delegate = delegate;		
 
-		//last position coordinates of mouse
-		_this.lastMousePosition = {x: 0, y: 0};
+		//utility
+		_this.utility = utility;
+
+		//boolean indicating whether first move was done or not
+		//helps preventing initial distance jump after init when mouse is in the center of the screen 
+		//and not in top left corner
+		_this.firstMove = false;
+
+		//last position coordinates of mouse		
+		_this.lastMousePosition = {x: 0, y: 0};		
 
 		/**
 		* Initializes tracking events and handler functions
@@ -114,13 +172,24 @@
 		* Triggered on 'mousemove' event
 		* @param array event - event data
 		*/
-		_this.handleMouseMove = function(event){			
-			var newPosition = {x: event.pageX, y: event.pageY};			
+		_this.handleMouseMove = function(event){									
+			var currentMousePosition = {x: event.pageX, y: event.pageY};			
+
+			//avoid first move jump
+			if(false === _this.firstMove){
+				_this.firstMove = true;
+				_this.lastMousePosition = currentMousePosition;
+				return;				
+			}
+
+			//calculate distance traveled
 			var distanceTraveled = _this.utility.computeDistance(
-				_this.lastMousePosition, newPosition
-			);
-			console.log(distanceTraveled);
-			_this.lastMousePosition = newPosition;			
+				_this.lastMousePosition, currentMousePosition
+			);			
+			_this.lastMousePosition = currentMousePosition;
+
+			//let delegate know
+			_this.delegate.distanceChanged(distanceTraveled);			
 		};		
 	};	
 
@@ -131,9 +200,35 @@
 		//this object
 		var _this = this;
 
-		_this.init = function(){
-					
+
+		/**
+		* Sets the key and value in storage
+		* @param string key - key to be set
+		* @param string value - value to be set
+		*/
+		_this.setKey = function(key, value){
+			window.localStorage[STORAGE_PREFIX + key] = value;
 		};
+
+		/**
+		* Retrieves value for the key from the storage
+		* @param string key - key to identify item
+		* @return string|boolean - value for the key or boolean false if the key is not set
+		*/
+		_this.getKey = function(key){
+			return window.localStorage[STORAGE_PREFIX + key] !== undefined
+				? window.localStorage[STORAGE_PREFIX + key] : false;
+		};
+
+		/**
+		* Removes a key from storage
+		* @param string key - key to be removed
+		*/
+		_this.deleteKey = function(key){
+			delete window.localStorage[STORAGE_PREFIX + key];
+		};
+
+
 	};
 
 	/**
@@ -152,8 +247,17 @@
 		*/
 		_this.computeDistance = function(pointX, pointY){
 			return Math.sqrt(
-				Math.pow((pointX.x - pointY.x), 2) + Math.pow(pointX.y - pointY.y, 2)
+				parseFloat(Math.pow((pointX.x - pointY.x), 2) + Math.pow(pointX.y - pointY.y, 2))
 			);
+		};
+
+		/**
+		* Conversion function. Converts PX to MM
+		* @param float pixels - PX units
+		* @return float - MM units
+		*/
+		_this.convertPixelsToMilimeters = function(pixels){
+			return (pixels * 2.54 / 96) * 10;
 		};
 	};
 
